0f93539f0d347ac2520045bd4a61e1d3aaa71786
tuui <them62@hotmail.com>
2015-03-26 23:18:28 +0200

    role selection for user edit

--- src/main/java/testapp/view/controller/UserEditController.java
+++ src/main/java/testapp/view/controller/UserEditController.java
@@ -1,5 +1,8 @@
 package testapp.view.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.apache.commons.lang3.StringUtils;
@@ -8,7 +11,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -22,12 +27,18 @@ import testapp.server.service.UserI;
 public class UserEditController {
 
 	private final static Logger log = LoggerFactory.getLogger(UserEditController.class);
+	private final static List<String> roles = Arrays.asList("ROLE_USER", "ROLE_ADMIN");
 	
 	@Autowired
 	private UserI userI;
 	
 	private String oldPassword;
 		
+	@ModelAttribute("allRoles")
+	public List<String> getAllRoles(){
+		return roles;
+	}
+	
 	//	User peab olema argumendiks, muidu tuleb: Neither BindingResult nor plain target object for bean name 'user'
 	@RequestMapping({"/admin/userEdit"})
     public String viewUserEdit(final User user, Model model) {
@@ -38,7 +49,7 @@ public class UserEditController {
     }
 	
 	@RequestMapping(value="/admin/userEdit", params={"saveUser"}, method = RequestMethod.POST)
-    public String saveUser(@Valid User user, BindingResult bindingResult, SessionStatus status, Model model) {	
+    public String saveUser(@Valid User user, BindingResult bindingResult, SessionStatus status, ModelMap model) {	
 		if(user.getPassword().isEmpty()){
 			user.setPassword(oldPassword);
 		}
@@ -47,12 +58,13 @@ public class UserEditController {
 			bindingResult.rejectValue("password", "Empty", "Password cannot be empty.");
 		}
 
-        if (bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors() || !roles.contains(user.getRole())) {
         	model.addAttribute("page", "users");
             return "admin/user/userEdit";
         }
         userI.saveUser(user);
         status.setComplete();
+        model.clear();
         return "redirect:/admin/userList";
     }
 
